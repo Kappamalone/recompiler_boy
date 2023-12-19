dc61a6bd521b23184917de2ca1c7c195b431b4cd
kappamalone <uzman.zawahir1@gmail.com>
2023-12-19 13:30:38 +1100

    update cmakelist to work and compile

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 mkdir -p build && cd build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE="$1"  ..
+cmake -Wno-dev -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE="$1"  ..
 ln -rsf compile_commands.json ..
 ninja
 ./TEMPLATE
--- src/core/common.h
+++ src/core/common.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "fmt/core.h"
+#include <cstdint>
 #include <vector>
 
 #ifndef NDEBUG
